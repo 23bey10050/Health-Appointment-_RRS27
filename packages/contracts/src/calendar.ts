d40b9004c8ed4bc93bcd5faa6946961a86f57738
23BEY10050 <ritu.23bey10050@vitbhopal.ac.in>
2026-08-24 01:10:54 +0530

    updating the tech stack due to backend issues

--- packages/contracts/src/calendar.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const googleConnectUrlResponseSchema = z.object({ url: z.string().url() });
-export type GoogleConnectUrlResponse = z.infer<typeof googleConnectUrlResponseSchema>;
-
-export const googleConnectionStatusSchema = z.object({ connected: z.boolean() });
-export type GoogleConnectionStatus = z.infer<typeof googleConnectionStatusSchema>;
-
-export const googleDisconnectResponseSchema = z.object({ disconnected: z.literal(true) });
